2f274b2d84cfed6b6ac947afd58b4cede5bbb4bf
harrypars0ns <harry.scound@hotmail.com>
2020-05-19 11:53:58 +0000

    Added form image select and favorites switch

---
+++ static/js/script.js
@@ -0,0 +1,17 @@
+// function helloWorld(){
+//     console.log('hello world');
+// }
+
+// helloWorld()
+
+
+// // function checkUrl(){
+    
+// //     var formImageUrl = document.getElementById("recipe_image").value;
+// //     if (formImageUrl.endsWith(".jpg", ".jpeg", ".png")){
+// //         console.log(true)
+// //     }
+// //     if else{
+// //         console.log(false)
+// //     }
+// // }
