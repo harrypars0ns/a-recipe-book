e74cffa0a567b01ecf8d0b5cc64d96a598c8510d
harrypars0ns <harry.scound@hotmail.com>
2020-06-15 11:04:26 +0000

    Deployment error

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-import os
-if os.path.exists("env.py"):
-    import env
-
 
 app = Flask(__name__)
 # ENVIRONMENT VARIABLES
